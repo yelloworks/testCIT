def80175c2f5d2920694adf3dd9d106801621421
a.rogov <a.rogov@intelprime.com>
2019-05-03 18:46:58 +0300

    oh shit

--- testCIT/testCIT/App_Start/BundleConfig.cs
+++ testCIT/testCIT/App_Start/BundleConfig.cs
@@ -22,6 +22,8 @@ namespace testCIT
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/angular.js"));
         }
     }
 }
---
+++ testCIT/testCIT/Scripts/UI/controller.js
@@ -0,0 +1,57 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('controller', controller);
+
+    controller.$inject = ['$scope', '$http', '$location'];
+
+    function controller($scope, $http, $location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'controller';
+
+        $scope.myData = [
+            {
+                firstName: "Cox",
+                lastName: "Carney",
+                company: "Enormo",
+                employed: true
+            },
+            {
+                firstName: "Lorraine",
+                lastName: "Wise",
+                company: "Comveyer",
+                employed: false
+            },
+            {
+                firstName: "Nancy",
+                lastName: "Waters",
+                company: "Fuelton",
+                employed: false
+            }
+        ];
+
+        $scope.onTmpButtonClick = function() {
+            alert("Works");
+        }
+
+
+        $http.get($location.$$absUrl + '/api/values').then(function(responce) {
+            var a = responce.data;
+        });
+
+
+        $http({ method: 'GET', url: $location.$$absUrl + '/api/values' }).
+            success(function (data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+                var a = data;
+            });
+
+        activate();
+
+        function activate() { }
+    }
+})();
